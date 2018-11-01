5983d2b115b03992c0f4da75dffd03d082123b70
克博 <479323208@qq.com>
2018-11-01 11:14:54 +0800

    提交

--- LearnJoinPods/ViewController.m
+++ LearnJoinPods/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking.h>
+#import <UIImageView+WebCache.h>
+#import <MJRefresh.h>
 
 @interface ViewController ()
 
@@ -17,7 +20,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    AFHTTPSessionManager * manager = [AFHTTPSessionManager manager];
+    [manager POST:@"url" parameters:@{@"key":@"value"} progress:^(NSProgress * _Nonnull uploadProgress) {
+        //进度
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        //成功回调
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        //失败回调
+    }];
+    
+    UIImageView * imageView = [[UIImageView alloc]initWithFrame:CGRectZero];
+    [imageView sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"imageURL"]] placeholderImage:[UIImage imageNamed:@"imagePlaceHolder"] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+        //图片加载完成后，需要执行什么操作
+    }];
+    
+    UITableView * tableView = [[UITableView alloc]initWithFrame:CGRectZero style:UITableViewStylePlain];
+    __weak typeof(self) weakSelf = self;
+    tableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        [weakSelf reloadData];
+    }];
 }
 
+- (void)reloadData
+{
+    //下拉刷新
+}
 
 @end
---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+
+
+
+target 'LearnJoinPods' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for LearnJoinPods
+  
+  pod'AFNetworking'
+  pod 'SDWebImage'
+  pod 'MJRefresh'
+
+  target 'LearnJoinPodsTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'LearnJoinPodsUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
